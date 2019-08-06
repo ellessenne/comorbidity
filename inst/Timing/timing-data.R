b5874de00a2aa873532c07d02820bc0265da260c
Alessandro Gasparini <ag475@le.ac.uk>
2019-08-06 09:55:21 +0100

    Updated script to simualate data for timings

--- inst/Timing/timing-data.R
+++ inst/Timing/timing-data.R
@@ -1,4 +1,4 @@
-### Timing released version 0.2.0 vs 0.1.3
+### Timing released version 0.4.1 vs #13 vs #13v2
 
 # Packages
 library(tidyverse)
@@ -6,14 +6,19 @@ library(devtools)
 
 # Defining data-generating mechanisms
 set.seed(20190806)
-dgms <- tidyr::crossing(
-  n_ids = stats::runif(n = 100, min = 1000, max = 1000000) %>% ceiling(),
-  n_codes = seq(from = 10, to = 50, by = 10)) %>% 
-  dplyr::mutate(ss = n_ids * n_codes)
+dgms <- tibble::tibble(
+  n_ids = stats::runif(n = 100, min = 1000, max = 1000000) %>% ceiling()
+)
+dgms$n_codes <- stats::runif(n = nrow(dgms), min = 10, max = 50) %>% ceiling()
+dgms$ss <- dgms$n_ids * dgms$n_codes
+dgms <- dgms %>%
+  dplyr::arrange(ss) %>%
+  dplyr::mutate(i = row_number())
+saveRDS(dgms, file = "/scratch/cvdanalysis/ag475/comorbidity-dgms.RDS")
 
 # Simulate data
 pb <- txtProgressBar(min = 0, max = nrow(dgms), style = 3)
-for (i in 1:nrow(dgms)) {
+for (i in dgms$i) {
   tmp <- data.frame(
     id = sample(1:dgms$n_ids[i], size = dgms$n_ids[i] * dgms$n_codes[i], replace = TRUE),
     code = comorbidity::sample_diag(dgms$n_ids[i] * dgms$n_codes[i]),
