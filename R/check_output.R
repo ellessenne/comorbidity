d1533466696ffe7722a95a58f3591b51a7b06908
Alessandro Gasparini <ellessenne@gmail.com>
2021-02-24 12:35:30 +0100

    Text edit

--- R/check_output.R
+++ R/check_output.R
@@ -2,5 +2,5 @@
 .check_output <- function(x, id) {
   x[[id]] <- NULL
   # Check that all identified comorbidity domains are either 0 or 1
-  if (!all(x == 0 | x == 1)) stop("'comorbidity' ended up in an unexpected state.\nPlease report a bug with a reproducible example at https://github.com/ellessenne/comorbidity/issues\n", call. = FALSE)
+  if (!all(x == 0 | x == 1)) stop("'comorbidity()' ended up in an unexpected state.\nPlease report a bug with a reproducible example at https://github.com/ellessenne/comorbidity/issues\n", call. = FALSE)
 }
