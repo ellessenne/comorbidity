c25ab2c49d8f3dd317f3922cc07632c07930f403
Alessandro Gasparini <ellessenne@gmail.com>
2021-07-08 18:01:00 +0200

    Reprex for testing

--- testing.R
+++ testing.R
@@ -1,21 +1,24 @@
-devtools::load_all()
-library(bench)
-library(ggplot2)
-library(ggbeeswarm)
+reprex::reprex({
+  library(comorbidity)
+  library(bench)
+  library(ggplot2)
+  library(ggbeeswarm)
+  library(tidyr)
 
-set.seed(1)
-x <- data.frame(
-  id = sample(seq(3e4), size = 3e6, replace = TRUE),
-  code = sample_diag(3e6),
-  stringsAsFactors = FALSE
-)
+  set.seed(1)
+  x <- data.frame(
+    id = sample(seq(1e3), size = 1e5, replace = TRUE),
+    code = sample_diag(1e5),
+    stringsAsFactors = FALSE
+  )
 
-addd <- sample(x = seq(nrow(x)), size = 3e4)
-x$code[addd] <- paste0(".", x$code[addd])
+  addd <- sample(x = seq(nrow(x)), size = 5e3)
+  x$code[addd] <- paste0(".", x$code[addd])
 
-bm <- bench::mark(
-  "current" = comorbidity(x = x, id = "id", code = "code", map = "charlson_icd10_quan", assign0 = FALSE, tidy.codes = TRUE),
-  "stringi" = comorbidity(x = x, id = "id", code = "code", map = "charlson_icd10_quan", assign0 = FALSE, stringi = TRUE, tidy.codes = TRUE),
-  iterations = 20
-)
-autoplot(bm)
+  bm <- bench::mark(
+    "current" = comorbidity(x = x, id = "id", code = "code", map = "charlson_icd10_quan", assign0 = FALSE, stringi = FALSE),
+    "stringi" = comorbidity(x = x, id = "id", code = "code", map = "charlson_icd10_quan", assign0 = FALSE, stringi = TRUE),
+    iterations = 30
+  )
+  bm
+}, venue = "gh")
