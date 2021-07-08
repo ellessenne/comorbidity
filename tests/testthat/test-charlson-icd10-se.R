38ac1fa247abed72798c2b0a91ba120220cb091e
Alessandro Gasparini <ellessenne@gmail.com>
2021-07-08 16:20:33 +0200

    No need to cat for debugging

--- tests/testthat/test-charlson-icd10-se.R
+++ tests/testthat/test-charlson-icd10-se.R
@@ -5,8 +5,7 @@ test_that("Charlson code, ICD-10 codes, Swedish version", {
   for (i in seq_along(this)) {
     cmb <- names(this)[i]
     for (j in seq_along(this[[i]])) {
-      # For debugging:
-      cat(this[[i]][j], "\n")
+      # For debugging: cat(this[[i]][j], "\n")
       x <- data.frame(id = 1, code = this[[i]][j])
       test <- comorbidity(x = x, id = "id", code = "code", map = "charlson_icd10_se", assign0 = FALSE)
       expect_equal(object = test[[cmb]], expected = 1)
--- tests/testthat/test-elixhauser-icd10-quan.R
+++ tests/testthat/test-elixhauser-icd10-quan.R
@@ -5,8 +5,7 @@ test_that("Elixhauser code, ICD-10 codes", {
   for (i in seq_along(this)) {
     cmb <- names(this)[i]
     for (j in seq_along(this[[i]])) {
-      # For debugging:
-      cat(this[[i]][j], "\n")
+      # For debugging: cat(this[[i]][j], "\n")
       x <- data.frame(id = 1, code = this[[i]][j])
       test <- comorbidity(x = x, id = "id", code = "code", map = "elixhauser_icd10_quan", assign0 = FALSE)
       if (x$code %in% c("I110", "I130", "I132", "I426", "G114", "I278", "I279", "I120", "I131", "K700", "K703", "K709", "F315", "F204")) {
