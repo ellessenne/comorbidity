039888856f33246d97969f68623da4ec799af310
Alessandro Gasparini <git@ellessenne.xyz>
2022-07-12 11:15:59 +0200

    Extra tests for #55

--- tests/testthat/test-#55.R
+++ tests/testthat/test-#55.R
@@ -30,3 +30,16 @@ test_that("character vs integer id makes no difference", {
     testthat::expect_equal(object = cmb1[, -which(names(cmb1) == "id")], expected = cmb2[, -which(names(cmb2) == "id")])
   }
 })
+
+test_that("character id with extra columns", {
+  n <- 15
+  N <- 100
+  x <- data.frame(
+    id = sample(seq(n), size = N, replace = TRUE),
+    code = sample_diag(N)
+  )
+  x$id <- as.character(x$id)
+  cmb <- comorbidity(x = x, id = "id", code = "code", map = "charlson_icd10_quan", assign0 = FALSE)
+  testthat::expect_equal(object = nrow(cmb), expected = n)
+  testthat::expect_s3_class(object = cmb, class = "data.frame")
+})
