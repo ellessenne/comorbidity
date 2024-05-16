2b164c4b97143710da772298d1378d6ca807488d
Alessandro Gasparini <git@ellessenne.xyz>
2024-05-16 14:45:30 +0200

    Warning showing up less frequently

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  tick <- stats::rbinom(n = 1, size = 1, prob = 0.1)
+  tick <- stats::rbinom(n = 1, size = 1, prob = 0.05)
   if (tick == 1) {
     packageStartupMessage(paste0("This is {comorbidity} version ", utils::packageVersion("comorbidity"), "."))
     packageStartupMessage("A lot has changed since the pre-1.0.0 release on CRAN, please check-out breaking changes here:")
